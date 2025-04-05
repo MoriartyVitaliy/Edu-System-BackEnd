06538e4706e88d24e47c856eb19b49eb42162bf4
Vitaliy Hriss <vgriss@knu.ua>
2025-04-05 15:08:57 +0300

    Add IScheduleInfoProvider interface with new methods
    Introduced a new namespace `Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IProviders`.
    Defined the `IScheduleInfoProvider` interface within this namespace.
    Added method signatures for creating daily schedules and retrieving weekly schedules.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Interfaces/IProviders/IScheduleInfoProvider.cs
@@ -0,0 +1,10 @@
+﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IProviders
+{
+    public interface IScheduleInfoProvider
+    {
+        Task CreateDailySchedulesForWeekAsync(Guid id, Guid schoolClassId, DateTime startOfNextWeek);
+        Task<WeeklySchedule> GetWeeklyScheduleByIdAsync(Guid weeklyScheduleId);
+    }
+}
