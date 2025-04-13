b36e8f0f20b0a19b6070e8dee2926d8109377565
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 00:29:41 +0300

    Add AlreadyExistsException for handling conflict scenarios
    A new `AlreadyExistsException` class has been added to the
    `Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions` namespace.
    This class inherits from `BaseException` and its constructor takes
    an optional `message` parameter with a default value of "Entity
    already exist". The constructor calls the base class constructor
    with the message, an HTTP status code of 409 (Conflict), and a
    description "Conflict situation via database". The `System.Net`
    namespace is imported to use the `HttpStatusCode` enumeration.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions
+{
+    public class AlreadyExistsException : BaseException
+    {
+        public AlreadyExistsException(string message = "Entity already exist") 
+            : base(message, (int)HttpStatusCode.Conflict, "Conflict situation via database")
+        {
+        }
+    }
+}
