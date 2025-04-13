49ff2032e79dacb31a6af3abff02f64a7560bf0b
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 01:09:00 +0300

    Add UserRepository and UserService classes
    Added UserRepository class implementing IUserRepository for DB operations using EF Core. Introduced UserService class implementing IUserService for user management, including password hashing and validation. Both classes utilize dependency injection and necessary using directives.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Repositories/UserRepository.cs
@@ -0,0 +1,70 @@
+﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IRepositories;
+using Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(Edu_System_BackEndDbContext context) : base(context)
+        {
+        }
+
+        public async Task AddAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users
+                .Include(u => u.UserRoles)
+                    .ThenInclude(ur => ur.Role)
+                .ToListAsync();
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .Include(u => u.UserRoles)
+                    .ThenInclude(ur => ur.Role)
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users
+                .Include(u => u.UserRoles)
+                    .ThenInclude(ur => ur.Role)
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            _context.Users.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UpdatePassword(Guid userId, string passwordHash)
+        {
+            var updated = await _context.Users
+                .Where(u => u.Id == userId)
+                .ExecuteUpdateAsync(u => u.SetProperty(x => x.PasswordHash, passwordHash));
+
+            return updated > 0;
+        }
+    }
+}
---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Services/UserService.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IRepositories;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Utils;
+using Microsoft.AspNetCore.Identity;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<object> _passwordHasher;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IPasswordHasher<object> passwordHasher, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(CreateUserDto createUserDto)
+        {
+            var user = await _userRepository.GetByEmailAsync(createUserDto.Email);
+            if (user != null)
+                throw new Exception("User already exists");
+
+            if (!PasswordValidator.IsValidPassword(createUserDto.Password))
+                throw new BadRequestException("Password must be at least 7 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.");
+
+            var passwordHash = _passwordHasher.HashPassword(new object(), createUserDto.Password);
+            var newUser = _mapper.Map<User>(createUserDto);
+
+            newUser.PasswordHash = passwordHash;
+
+            await _userRepository.AddAsync(newUser);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _userRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllAsync()
+        {
+            var users = await _userRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<UserDto>>(users);
+        }
+
+        public async Task<UserDto?> GetByEmailAsync(string email)
+        {
+            var user = await _userRepository.GetByEmailAsync(email);
+
+            if (user is null)
+                throw new NotFoundException("User not found");
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public async Task<UserDto?> GetByIdAsync(Guid id)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+            if (user is null)                           
+                throw new NotFoundException("User not found");
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public async Task UpdateAsync(UpdateUserDto updateUserDto)
+        {
+            var user = await _userRepository.GetByIdAsync(updateUserDto.Id);
+
+            if (user is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+
+            _mapper.Map(updateUserDto, user);
+
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task UpdatePassword(Guid id, string passwordHash)
+        {
+            var user = await _userRepository.GetByIdAsync(id);
+
+            if (user is null)
+            {
+                throw new NotFoundException("User not found.");
+            }
+            await _userRepository.UpdatePassword(id, passwordHash);
+        }
+    }
+}
