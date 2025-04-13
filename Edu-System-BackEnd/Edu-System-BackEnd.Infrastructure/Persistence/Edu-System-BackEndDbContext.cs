7ff9d86dda02ac8efb3305786c01a3609c39ee77
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 00:35:13 +0300

    Refactor and enhance entity configurations with Fluent API
    Updated Edu-System-BackEndDbContext to include detailed entity configurations using the Fluent API. Added configurations for User, Teacher, Student, and Parent entities, specifying primary keys, required properties, relationships, and delete behaviors. Refactored configurations for UserRole, StudentParent, SchoolClass, Lesson, Homework, HomeworkSubmission, DailySchedule, and WeeklySchedule entities to use the Fluent API, ensuring proper relationships and constraints.

--- Edu-System-BackEnd/Edu-System-BackEnd.Infrastructure/Persistence/Edu-System-BackEndDbContext.cs
+++ Edu-System-BackEnd/Edu-System-BackEnd.Infrastructure/Persistence/Edu-System-BackEndDbContext.cs
@@ -38,134 +38,233 @@ namespace Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence
             modelBuilder.Entity<Parent>().ToTable("parents");
             modelBuilder.Entity<Teacher>().ToTable("teachers");
 
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.MiddleName).HasMaxLength(100);
+                entity.Property(u => u.LastName).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.Email).IsRequired().HasMaxLength(255);
+                entity.Property(u => u.PasswordHash).IsRequired();
+                entity.Property(u => u.PhoneNumber).HasMaxLength(20);
+            });
+
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.HasKey(t => t.UserId);
+
+                entity.HasOne(t => t.User)
+                      .WithOne()
+                      .HasForeignKey<Teacher>(t => t.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(t => t.Lessons)
+                      .WithOne(l => l.Teacher)
+                      .HasForeignKey(l => l.TeacherId);
+
+                entity.HasMany(t => t.ClassSupervisions)
+                      .WithOne(c => c.Teacher)
+                      .HasForeignKey(c => c.TeacherId)
+                      .OnDelete(DeleteBehavior.SetNull); // если нужно сохранять класс без куратора
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(s => s.UserId);
+
+                entity.HasOne(s => s.User)
+                      .WithOne()
+                      .HasForeignKey<Student>(s => s.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(s => s.SchoolClass)
+                      .WithMany(c => c.Students)
+                      .HasForeignKey(s => s.SchoolClassId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(s => s.LessonMarks)
+                      .WithOne(m => m.Student)
+                      .HasForeignKey(m => m.StudentId);
+
+                entity.HasMany(s => s.HomeworkMarks)
+                      .WithOne(m => m.Student)
+                      .HasForeignKey(m => m.StudentId);
+
+                entity.HasMany(s => s.Attendances)
+                      .WithOne(a => a.Student)
+                      .HasForeignKey(a => a.StudentId);
+
+                entity.HasMany(s => s.HomeworkSubmissions)
+                      .WithOne(h => h.Student)
+                      .HasForeignKey(h => h.StudentId);
+            });
+
+            modelBuilder.Entity<Parent>(entity =>
+            {
+                entity.HasKey(p => p.UserId);
+
+                entity.HasOne(p => p.User)
+                      .WithOne()
+                      .HasForeignKey<Parent>(p => p.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+
+
+
             //User Roles
-            modelBuilder.Entity<UserRole>()
-                .HasKey(ur => new { ur.UserId, ur.RoleId });
 
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.User)
-                .WithMany(u => u.UserRoles)
-                .HasForeignKey(ur => ur.UserId);
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.HasKey(ur => new { ur.UserId, ur.RoleId });
 
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.Role)
-                .WithMany(r => r.UserRoles)
-                .HasForeignKey(ur => ur.RoleId);
+                entity.HasOne(ur => ur.User)
+                      .WithMany(u => u.UserRoles)
+                      .HasForeignKey(ur => ur.UserId);
+
+                entity.HasOne(ur => ur.Role)
+                      .WithMany(r => r.UserRoles)
+                      .HasForeignKey(ur => ur.RoleId);
+            });
+
+
+            modelBuilder.Entity<StudentParent>(entity =>
+            {
+                entity.HasKey(sp => new { sp.StudentId, sp.ParentId });
+
+                entity.HasOne(sp => sp.Student)
+                      .WithMany(s => s.StudentParents)
+                      .HasForeignKey(sp => sp.StudentId);
+
+                entity.HasOne(sp => sp.Parent)
+                      .WithMany(p => p.StudentParents)
+                      .HasForeignKey(sp => sp.ParentId);
+            });
 
             //Student Parents
-            modelBuilder.Entity<StudentParent>()
-                .HasKey(sp => new { sp.StudentId, sp.ParentId });
 
-            modelBuilder.Entity<StudentParent>()
-                .HasOne(sp => sp.Student)
-                .WithMany(s => s.StudentParents)
-                .HasForeignKey(sp => sp.StudentId);
-
-            modelBuilder.Entity<StudentParent>()
-                .HasOne(sp => sp.Parent)
-                .WithMany(p => p.StudentParents)
-                .HasForeignKey(sp => sp.ParentId);
 
             //School Class
-            modelBuilder.Entity<SchoolClass>()
-                .HasMany(sc => sc.Students)
-                .WithOne(s => s.SchoolClass)
-                .HasForeignKey(s => s.SchoolClassId)
-                .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<SchoolClass>()
-                .HasOne(sc => sc.Teacher)
-                .WithMany(t => t.ClassSupervisions)
-                .HasForeignKey(sc => sc.TeacherId)
-                .OnDelete(DeleteBehavior.SetNull);
+            modelBuilder.Entity<SchoolClass>(entity =>
+            {
+                entity.HasKey(sc => sc.Id);
+
+                entity.HasMany(sc => sc.Students)
+                      .WithOne(s => s.SchoolClass)
+                      .HasForeignKey(s => s.SchoolClassId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(sc => sc.Teacher)
+                      .WithMany(t => t.ClassSupervisions)
+                      .HasForeignKey(sc => sc.TeacherId)
+                      .OnDelete(DeleteBehavior.SetNull);
+            });
+
+
+
+            modelBuilder.Entity<Lesson>(entity =>
+            {
+                entity.HasKey(l => l.Id);
+
+                entity.HasOne(l => l.SchoolClass)
+                      .WithMany(sc => sc.Lessons)
+                      .HasForeignKey(l => l.SchoolClassId)
+                      .OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(s => s.Subject)
+                      .WithMany(s => s.Lessons)
+                      .HasForeignKey(l => l.SubjectId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(t => t.Teacher)
+                      .WithMany(t => t.Lessons)
+                      .HasForeignKey(l => l.TeacherId);
+
+                entity.HasMany(a => a.Attendances)
+                      .WithOne(l => l.Lesson)
+                      .HasForeignKey(a => a.LessonId);
+
+                entity.HasMany(lm => lm.LessonMarks)
+                      .WithOne(l => l.Lesson)
+                      .HasForeignKey(lm => lm.LessonId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(hm => hm.Homeworks)
+                      .WithOne(l => l.Lesson)
+                      .HasForeignKey(hm => hm.LessonId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(ds => ds.DailySchedule)
+                      .WithMany(l => l.Lessons)
+                      .HasForeignKey(ds => ds.DailyScheduleId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
 
             //Lesson
-            modelBuilder.Entity<Lesson>()
-                .HasOne(l => l.SchoolClass)
-                .WithMany(sc => sc.Lessons)
-                .HasForeignKey(l => l.SchoolClassId)
-                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<Lesson>()
-                .HasOne(s => s.Subject)
-                .WithMany(s => s.Lessons)
-                .HasForeignKey(l => l.SubjectId)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Lesson>()
-                .HasOne(t => t.Teacher)
-                .WithMany(t => t.Lessons)
-                .HasForeignKey(l => l.TeacherId);
-
-            modelBuilder.Entity<Lesson>()
-                .HasMany(a => a.Attendances)
-                .WithOne(l => l.Lesson)
-                .HasForeignKey(a => a.LessonId);
-
-            modelBuilder.Entity<Lesson>()
-                .HasMany(lm => lm.LessonMarks)
-                .WithOne(l => l.Lesson)
-                .HasForeignKey(lm => lm.LessonId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Lesson>()
-                .HasMany(hm => hm.Homeworks)
-                .WithOne(l => l.Lesson)
-                .HasForeignKey(hm => hm.LessonId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Lesson>()
-                .HasOne(ds => ds.DailySchedule)
-                .WithMany(l => l.Lessons)
-                .HasForeignKey(ds => ds.DailyScheduleId)
-                .OnDelete(DeleteBehavior.Cascade);
 
             //Homework
 
-            modelBuilder.Entity<Homework>()
-                .HasMany(hs => hs.Submissions)
-                .WithOne(h => h.Homework)
-                .HasForeignKey(hs => hs.HomeworkId);
+            modelBuilder.Entity<Homework>(entity =>
+            { 
+                entity.HasMany(hs => hs.Submissions)
+                      .WithOne(h => h.Homework)
+                      .HasForeignKey(hs => hs.HomeworkId);
+            });
 
             //Homework Submission
 
-            modelBuilder.Entity<HomeworkSubmission>()
-                .HasOne(hs => hs.Student)
-                .WithMany(s => s.HomeworkSubmissions)
-                .HasForeignKey(hs => hs.StudentId)
-                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<HomeworkSubmission>(entity =>
+            {
+                entity.HasKey(hs => hs.Id);
 
-            modelBuilder.Entity<HomeworkSubmission>()
-                .HasOne(hs => hs.Homework)
-                .WithMany(h => h.Submissions)
-                .HasForeignKey(hs => hs.HomeworkId)
-                .OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(hs => hs.Student)
+                      .WithMany(s => s.HomeworkSubmissions)
+                      .HasForeignKey(hs => hs.StudentId)
+                      .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<HomeworkSubmission>()
-                .HasOne(hs => hs.HomeworkMark)
-                .WithOne(m => m.HomeworkSubmission)
-                .HasForeignKey<HomeworkSubmission>(hs => hs.HomeworkMarkId)
-                .OnDelete(DeleteBehavior.Cascade);
+                entity.HasOne(hs => hs.Homework)
+                      .WithMany(h => h.Submissions)
+                      .HasForeignKey(hs => hs.HomeworkId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(hs => hs.HomeworkMark)
+                      .WithOne(m => m.HomeworkSubmission)
+                      .HasForeignKey<HomeworkSubmission>(hs => hs.HomeworkMarkId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
 
             //DailySchedule
-            modelBuilder.Entity<DailySchedule>()
-                .HasOne(sc => sc.SchoolClass)
-                .WithMany(s => s.Schedules)
-                .HasForeignKey(sc => sc.SchoolClassId)
-                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<DailySchedule>()
-                .HasOne(s => s.SchoolClass)
-                .WithMany(sc => sc.Schedules)
-                .HasForeignKey(s => s.SchoolClassId)
-                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<DailySchedule>(entity =>
+            {
+                entity.HasKey(ds => ds.Id);
+
+                entity.HasOne(sc => sc.SchoolClass)
+                      .WithMany(s => s.Schedules)
+                      .HasForeignKey(sc => sc.SchoolClassId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(s => s.SchoolClass)
+                      .WithMany(sc => sc.Schedules)
+                      .HasForeignKey(s => s.SchoolClassId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+
+
 
             //WeeklySchedule
-            modelBuilder.Entity<WeeklySchedule>()
-                .HasMany(ws => ws.DailySchedules)
-                .WithOne(w => w.WeeklySchedule)
-                .HasForeignKey(ws => ws.WeeklyScheduleId)
-                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<WeeklySchedule>(entity =>
+            {
+                entity.HasMany(ws => ws.DailySchedules)
+                      .WithOne(w => w.WeeklySchedule)
+                      .HasForeignKey(ws => ws.WeeklyScheduleId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
