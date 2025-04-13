f77665b2b3611d0b97782bbbcadd7dd5dc2e0b23
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 00:30:49 +0300

    Add LoginDto class for login data in new namespace
    Introduced a new namespace `Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Login`.
    Within this namespace, added a `LoginDto` class with two nullable string properties: `Email` and `Password`.
    This DTO is intended to encapsulate login-related data.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/DTOs/Login/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Login
+{
+    public class LoginDto
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
