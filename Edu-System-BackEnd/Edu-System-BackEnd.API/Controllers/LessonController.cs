13ccee59d7bd813c7a36a005c9ce0f30c61ec2da
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 01:12:05 +0300

    Add authorization and role-based access to LessonController
    Enhanced LessonController with authorization features:
    - Added [Authorize] attribute at the class level for default authentication.
    - Allowed anonymous access to GetLessonsAsync and GetLessonByIdAsync.
    - Restricted AddLessonAsync, UpdateLessonAsync, and DeleteLessonAsync to Admin and Teacher roles.
    - Improved security and access management for endpoints.

--- Edu-System-BackEnd/Edu-System-BackEnd.API/Controllers/LessonController.cs
+++ Edu-System-BackEnd/Edu-System-BackEnd.API/Controllers/LessonController.cs
@@ -1,10 +1,12 @@
 ﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Schedule;
 using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
 using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Edu_System_BackEnd.Edu_System_BackEnd.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/v1/[controller]")]
     public class LessonController : ControllerBase
@@ -14,12 +16,16 @@ namespace Edu_System_BackEnd.Edu_System_BackEnd.API.Controllers
         {
             _lessonService = lessonService;
         }
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetLessonsAsync()
         {
             var lessons = await _lessonService.GetAllLessonsAsync();
             return Ok(lessons);
         }
+
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetLessonByIdAsync(Guid id)
         {
@@ -29,12 +35,16 @@ namespace Edu_System_BackEnd.Edu_System_BackEnd.API.Controllers
 
             return Ok(lesson);
         }
+
+        [Authorize(Roles = "Admin,Teacher")]
         [HttpPost]
         public async Task<IActionResult> AddLessonAsync([FromBody] CreateLessonDto createLessonDto)
         {
             var lessonDto = await _lessonService.CreateLessonAsync(createLessonDto);
             return CreatedAtAction(nameof(GetLessonByIdAsync), new { id = lessonDto.Id }, createLessonDto);
         }
+
+        [Authorize(Roles = "Admin,Teacher")]
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateLessonAsync(Guid id, [FromBody] UpdateLessonDto updateLessonDto)
         {
@@ -44,6 +54,8 @@ namespace Edu_System_BackEnd.Edu_System_BackEnd.API.Controllers
             await _lessonService.UpdateLessonAsync(updateLessonDto);
             return NoContent();
         }
+
+        [Authorize(Roles = "Admin,Teacher")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteLessonAsync(Guid id)
         {
