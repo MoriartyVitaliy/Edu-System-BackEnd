f89e420b6822395748d3e15556cebd5beac9be91
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 01:10:41 +0300

    Refactor StudentRepository and update StudentService
    Refactored StudentRepository with new methods and removed outdated ones. Updated StudentService to reflect repository changes and added new functionalities, including user and role management.

--- Edu-System-BackEnd/Edu-System-BackEnd.Core/Repositories/StudentRepository.cs
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Repositories/StudentRepository.cs
@@ -6,75 +6,101 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Repositories
 {
-
-
-    //TODO : Implement methods and add exceptions to them
     public class StudentRepository : BaseRepository, IStudentRepository
     {
         public StudentRepository(Edu_System_BackEndDbContext context) : base(context) { }
-        public async Task<IEnumerable<Student>> GetAllAsync()
-        {
-            return await _context.Students
-                .Include(s => s.SchoolClass)
-                .Include(s => s.UserRoles)
-                    .ThenInclude(ur => ur.Role)
-                .ToListAsync();
-        }
-        public async Task<Student?> GetByIdAsync(Guid id)
-        {
-            return await _context.Students
-                .Include(s => s.SchoolClass)
-                .Include(s => s.UserRoles)
-                    .ThenInclude(ur => ur.Role)
-                .FirstOrDefaultAsync(s => s.Id == id);
-        }
+
         public async Task AddAsync(Student student)
         {
-            var studentRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "Student");
-
-            if (studentRole == null)
-                throw new NotFoundException("Role 'Student' not found via database.");
-            
-
-            student.UserRoles = new List<UserRole> { new UserRole { RoleId = studentRole.Id, UserId = student.Id } };
-            var schoolClass = await _context.SchoolClasses.FirstOrDefaultAsync(sc => sc.Id == student.SchoolClassId);
-            if (schoolClass == null)
-                throw new NotFoundException("School class not found.");
-
-            student.SchoolClass = schoolClass;
-
             await _context.Students.AddAsync(student);
-
             await _context.SaveChangesAsync();
         }
-        public async Task UpdateAsync(Student student)
+
+        public async Task AddParentAsync(Guid studentId, Guid parentId)
         {
-            var existingStudent = await _context.Students.FirstOrDefaultAsync(s => s.Id == student.Id);
-            if (existingStudent == null)
-                throw new NotFoundException($"Student with ID {student.Id} not found.");
+            var student = await _context.Students
+                .Include(s => s.StudentParents)
+                .FirstOrDefaultAsync(s => s.UserId == studentId);
 
+            if (student == null)
+                throw new NotFoundException("Student not found");
 
-            _context.Students.Update(student);
+            var parent = await _context.Parents.FindAsync(parentId);
+            if (parent == null)
+                throw new NotFoundException("Parent not found");
+
+            var alreadyLinked = student.StudentParents
+                .Any(sp => sp.ParentId == parentId);
+
+            if (alreadyLinked)
+                return;
+
+            var relation = new StudentParent
+            {
+                Id = Guid.NewGuid(),
+                StudentId = studentId,
+                ParentId = parentId
+            };
+
+            await _context.StudentParents.AddAsync(relation);
             await _context.SaveChangesAsync();
         }
-        public async Task UpdateStudentClassAsync(Guid studentId, Guid classId)
-        {
-            var student = await _context.Students.FindAsync(studentId)
-                ?? throw new NotFoundException("Student not found.");
 
-            var schoolClass = await _context.SchoolClasses.FindAsync(classId)
-                ?? throw new NotFoundException("School class not found.");
 
-            student.SchoolClass = schoolClass;
-            await _context.SaveChangesAsync();
-        }
+
         public async Task DeleteAsync(Guid id)
         {
-            var student = await _context.Students.FindAsync(id)
-                ?? throw new NotFoundException($"Student with ID {id} not found.");
-            
+            var student = await _context.Students.FirstOrDefaultAsync(p => p.UserId == id);
+            if (student == null)
+                throw new NotFoundException("Student not found");
+
             _context.Students.Remove(student);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<IEnumerable<Student>> GetAllAsync()
+        {
+            return await _context.Students
+                .Include(s => s.User)
+                    .ThenInclude(u => u.UserRoles)
+                        .ThenInclude(ur => ur.Role)
+                .Include(s => s.SchoolClass)
+                .Include(s => s.StudentParents)
+                    .ThenInclude(sp => sp.Parent)
+                .ToListAsync();
+        }
+
+
+        public async Task<Student?> GetByIdAsync(Guid id)
+        {
+            return await _context.Students
+                .Include(s => s.User)
+                    .ThenInclude(u => u.UserRoles)
+                        .ThenInclude(ur => ur.Role)
+                .Include(s => s.SchoolClass)
+                .Include(s => s.StudentParents)
+                    .ThenInclude(sp => sp.Parent)
+                .FirstOrDefaultAsync(s => s.UserId == id);
+        }
+
+        public Task UpdateAsync(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateClassAsync(Guid studentId, Guid newClassId)
+        {
+            var student = await _context.Students.FindAsync(studentId);
+            if (student == null)
+                throw new NotFoundException("Student not found");
+
+            var schoolClass = await _context.SchoolClasses.FindAsync(newClassId);
+            if (schoolClass == null)
+                throw new NotFoundException("Class not found");
+
+            student.SchoolClassId = newClassId;
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
--- Edu-System-BackEnd/Edu-System-BackEnd.Core/Services/StudentService.cs
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Services/StudentService.cs
@@ -3,55 +3,79 @@ using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs;
 using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
 using Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions;
 using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Utils;
+using Microsoft.AspNetCore.Identity;
 
 namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Services
 {
     public class StudentService : IStudentService
     {
+        private readonly IUserService _userService;
+        private readonly IRoleService _roleService;
         private readonly IStudentRepository _studentRepository;
         private readonly IMapper _mapper;
-        public StudentService(IStudentRepository studentRepository, IMapper mapper)
+        public StudentService(IUserService userService, IStudentRepository studentRepository, IMapper mapper, IRoleService roleService)
         {
+            _userService = userService;
             _studentRepository = studentRepository;
             _mapper = mapper;
+            _roleService = roleService;
         }
-        public async Task<IEnumerable<StudentDto>> GetAllStudentsAsync()
+
+        public async Task AddAsync(CreateStudentDto createStudentDto)
+        {
+            await _userService.AddAsync(createStudentDto.User);
+
+            var student = await _userService.GetByEmailAsync(createStudentDto.User.Email)
+                ?? throw new NotFoundException($"User with email {createStudentDto.User.Email} not found.");
+
+            await _roleService.AssignUserRole(student.Id, "Student");
+
+            var studentEntity = _mapper.Map<Student>(createStudentDto);
+            
+            studentEntity.UserId = student.Id;
+            studentEntity.User = null;
+
+            await _studentRepository.AddAsync(studentEntity);
+        }
+
+        public async Task AddOrUpdateParentAsync(Guid studentId, Guid parentId)
+        {
+            await _studentRepository.AddParentAsync(studentId, parentId);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _userService.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<StudentDto>> GetAllAsync()
         {
             var students = await _studentRepository.GetAllAsync();
             return _mapper.Map<IEnumerable<StudentDto>>(students);
         }
-        public async Task<StudentDto?> GetStudentByIdAsync(Guid id)
-        {
-            var students = await _studentRepository.GetByIdAsync(id)
-                ?? throw new NotFoundException($"Student with id {id} not found");
-            
-            return _mapper.Map<StudentDto>(students);
-        }
-        public async Task<StudentDto> CreateStudentAsync(CreateStudentDto createStudentDto)
-        {
-            var student = _mapper.Map<Student>(createStudentDto);
-            student.Id = Guid.NewGuid();
 
-            await _studentRepository.AddAsync(student);
+        public async Task<StudentDto?> GetByIdAsync(Guid id)
+        {
+            var student = await _studentRepository.GetByIdAsync(id)
+                ?? throw new NotFoundException($"Student with ID {id} not found.");
+
             return _mapper.Map<StudentDto>(student);
         }
-        public async Task UpdateStudentAsync(UpdateStudentDto updateStudentDto)
-        {
-            var student = await _studentRepository.GetByIdAsync(updateStudentDto.Id);
-            if (student == null)
-                throw new NotFoundException($"Student with ID {updateStudentDto.Id} not found.");
 
-            _mapper.Map(updateStudentDto, student);
+        public async Task UpdateAsync(UpdateStudentDto updateStudentDto)
+        {
+            await _userService.UpdateAsync(updateStudentDto.User);
+
+            var student = _mapper.Map<Student>(updateStudentDto);
 
             await _studentRepository.UpdateAsync(student);
         }
-        public async Task UpdateStudentClassAsync(Guid studentId, Guid classId)
+
+        public Task UpdateClassAsync(Guid studentId, Guid newClassId)
         {
-            await _studentRepository.UpdateStudentClassAsync(studentId, classId);
-        }
-        public async Task DeleteStudentAsync(Guid id)
-        {
-            await _studentRepository.DeleteAsync(id);
+            return _studentRepository.UpdateClassAsync(studentId, newClassId);
         }
     }
 }
