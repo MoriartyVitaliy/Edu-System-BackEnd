6f10c367c5156213b6ea5289af273e3117b26744
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 01:08:36 +0300

    Add AuthController and AuthService for authentication
    Introduce AuthController with Login endpoint in AuthController.cs.
    Add AuthService in AuthService.cs to handle authentication logic.
    AuthService depends on IUserRepository, IConfiguration, IPasswordHasher,
    and ILoginAttemptProvider interfaces. Implement LoginAsync method to
    validate credentials, check account lockouts, and generate JWT tokens.
    GenerateJwtToken method creates and signs JWT tokens with user claims.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Login;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var token = await _authService.LoginAsync(loginDto);
+            return Ok(new { message = token });
+        }
+    }
+}
---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Services/AuthService.cs
@@ -0,0 +1,92 @@
+﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IRepositories;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using Microsoft.AspNetCore.Identity;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using System.Security.Claims;
+using System.Text;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Login;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IProviders;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IConfiguration _config;
+        private readonly IPasswordHasher<object> _passwordHasher;
+        private readonly ILoginAttemptProvider _loginAttemptProvider;
+
+        public AuthService(
+            IUserRepository userRepository,
+            IConfiguration config,
+            IPasswordHasher<object> passwordHasher,
+            ILoginAttemptProvider loginAttemptProvider)
+        {
+            _userRepository = userRepository;
+            _config = config;
+            _passwordHasher = passwordHasher;
+            _loginAttemptProvider = loginAttemptProvider;
+        }
+        public async Task<string> LoginAsync(LoginDto login)
+        {
+            if (login.Email == null || login.Password == null)
+                throw new ArgumentNullException("Email and Password cannot be null.");
+
+            if (await _loginAttemptProvider.IsAccountLockedAsync(login.Email))
+                throw new UnauthorizedAccessException("Your account is temporarily locked due to multiple failed login attempts.");
+
+
+            var user = await _userRepository.GetByEmailAsync(login.Email);
+
+            if (user == null)
+            {
+                await _loginAttemptProvider.RecordFailedLoginAsync(login.Email);
+                throw new UnauthorizedAccessException("Invalid email or password.");
+            }
+
+            var result = _passwordHasher.VerifyHashedPassword(new object(), user.PasswordHash, login.Password);
+            if (result == PasswordVerificationResult.Failed)
+            {
+                await _loginAttemptProvider.RecordFailedLoginAsync(login.Email);
+                throw new UnauthorizedAccessException("Invalid email or password.");
+            }
+
+            await _loginAttemptProvider.RecordSuccessfulLoginAsync(login.Email);
+
+
+            return GenerateJwtToken(user);
+        }
+        private string GenerateJwtToken(User user)
+        {
+            var jwtSettings = _config.GetSection("JwtSettings");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Email),
+            };
+
+            // Додаємо ролі:
+            foreach (var role in user.UserRoles.Select(r => r.Role.Name))
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(jwtSettings["ExpiresInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
