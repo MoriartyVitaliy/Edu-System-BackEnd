fba0bba8d89e677ee5be46ce3e6d93b78ca88b37
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 01:07:34 +0300

    Add RoleRepository and RoleService implementations
    Added RoleRepository to manage roles with methods for CRUD operations and user role assignments. Introduced RoleService for higher-level role management, utilizing IRoleRepository for data access and IMapper for DTO-entity mapping.

---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Repositories/RoleRepository.cs
@@ -0,0 +1,99 @@
+﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IRepositories;
+using Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Role = Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities.Role;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Repositories
+{
+    public class RoleRepository : BaseRepository, IRoleRepository
+    {
+        public RoleRepository(Edu_System_BackEndDbContext context) : base(context) { }
+        public async Task AddAsync(Role entity)
+        {
+            await _context.Roles.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var role = await _context.Roles.FirstOrDefaultAsync(r => r.Id == id);
+            if (role == null)
+                throw new NotFoundException($"Role with ID {id} not found.");
+
+            _context.Roles.Remove(role);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Role>> GetAllAsync()
+        {
+            return await _context.Roles
+                .Include(r => r.UserRoles)
+                    .ThenInclude(ur => ur.User)
+                .ToListAsync();
+        }
+
+        public async Task<Role?> GetByIdAsync(Guid id)
+        {
+            return await _context.Roles
+                .Include(r => r.UserRoles)
+                    .ThenInclude(ur => ur.User)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task<Role?> GetRoleByNameAsync(string roleName)
+        {
+            return await _context.Roles
+                .Include(r => r.UserRoles)
+                    .ThenInclude(ur => ur.User)
+                .FirstOrDefaultAsync(r => r.Name == roleName);
+        }
+
+        public async Task<IEnumerable<Role>> GetRolesByUserIdAsync(Guid userId)
+        {
+            return await _context.Roles
+                .Include(r => r.UserRoles)
+                    .ThenInclude(ur => ur.User)
+                .Where(r => r.UserRoles.Any(ur => ur.UserId == userId))
+                .ToListAsync();
+        }
+
+        public Task<bool> IsUserInRoleAsync(Guid userId, string roleName)
+        {
+            return _context.UserRoles
+                .AnyAsync(ur => ur.UserId == userId && ur.Role.Name == roleName);
+        }
+
+        public async Task UpdateAsync(Role entity)
+        {
+            _context.Roles.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<bool> IsRoleExist(string roleName)
+        {
+            return await _context.Roles
+                .AnyAsync(r => r.Name == roleName);
+        }
+
+        public async Task<bool> AssignUserRoleAsync(Guid userId, Role role)
+        {
+            var userRole = new UserRole { UserId = userId, RoleId = role.Id };
+            _context.UserRoles.Add(userRole);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> RemoveUserRoleAsync(Guid userId, Role role)
+        {
+            var userRole = await _context.UserRoles
+                .FirstOrDefaultAsync(ur => ur.UserId == userId && ur.RoleId == role.Id);
+            if (userRole == null)
+                throw new NotFoundException($"UserRole with UserId {userId} and RoleId {role.Id} not found.");
+
+            _context.UserRoles.Remove(userRole);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Edu-System-BackEnd/Edu-System-BackEnd.Core/Services/RoleService.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.DTOs.Role;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Exceptions;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IRepositories;
+using Edu_System_BackEnd.Edu_System_BackEnd.Core.Interfaces.IServices;
+using StackExchange.Redis;
+using Role = Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities.Role;
+
+namespace Edu_System_BackEnd.Edu_System_BackEnd.Core.Services
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+        private readonly IMapper _mapper;
+        public RoleService(IRoleRepository roleRepository, IMapper mapper)
+        {
+            _roleRepository = roleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> AssignUserRole(Guid userId, string roleName)
+        {
+            var role = await _roleRepository.GetRoleByNameAsync(roleName);
+
+            if (role == null)
+                throw new NotFoundException($"Role '{roleName}' not found.");
+
+            if(await _roleRepository.IsUserInRoleAsync(userId, roleName))
+                throw new AlreadyExistsException($"User '{userId}' already has the role '{roleName}'.");
+
+            return await _roleRepository.AssignUserRoleAsync(userId, role);
+        }
+
+        public async Task<RoleDto> CreateRoleAsync(CreateRoleDto createRoleDto)
+        {
+            if(await _roleRepository.IsRoleExist(createRoleDto.Name))
+                throw new AlreadyExistsException($"Role '{createRoleDto.Name}' already exists.");
+            
+            var role = _mapper.Map<Role>(createRoleDto);
+            role.Id = Guid.NewGuid();
+
+            await _roleRepository.AddAsync(role);
+            return _mapper.Map<RoleDto>(role);
+        }
+
+        public async Task DeleteRoleAsyncById(Guid roleId)
+        {
+            await _roleRepository.DeleteAsync(roleId);
+        }
+
+        public async Task<IEnumerable<RoleDto>> GetAllRolesAsync()
+        {
+            var roles = await _roleRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<RoleDto>>(roles);
+        }
+
+        public async Task<RoleDto?> GetRoleByIdAsync(Guid roleId)
+        {
+            var role = await _roleRepository.GetByIdAsync(roleId)
+                ??  throw new NotFoundException($"Role with ID {roleId} not found.");
+
+            return _mapper.Map<RoleDto>(role);
+        }
+
+        public async Task<RoleDto?> GetRoleByNameAsync(string roleName)
+        {
+            var role = await _roleRepository.GetRoleByNameAsync(roleName)
+                ?? throw new NotFoundException($"Role with name {roleName} not found.");
+            return _mapper.Map<RoleDto>(role);
+        }
+
+        public async Task<IEnumerable<RoleDto>> GetRolesByUserIdAsync(Guid userId)
+        {
+            var roles = await _roleRepository.GetRolesByUserIdAsync(userId);
+            return _mapper.Map<IEnumerable<RoleDto>>(roles);
+        }
+
+        public async Task<bool> RemoveUserRoleAsync(Guid userId, string roleName)
+        {
+            var role = await _roleRepository.GetRoleByNameAsync(roleName);
+
+            if (role == null)
+                throw new NotFoundException($"Role '{roleName}' not found.");
+
+            return await _roleRepository.RemoveUserRoleAsync(userId, role);
+        }
+
+        public async Task UpdateRoleAsync(UpdateRoleDto roleDto)
+        {
+            if (await _roleRepository.IsRoleExist(roleDto.Name))
+                throw new AlreadyExistsException($"Role with name '{roleDto.Name}' already exists.");
+            
+
+            var role = _mapper.Map<Role>(roleDto);
+
+            await _roleRepository.UpdateAsync(role);
+        }
+    }
+}
