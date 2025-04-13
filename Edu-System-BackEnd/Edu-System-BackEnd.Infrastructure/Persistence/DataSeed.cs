e390befbecbb8a3976c654b25925174e36453b8e
Vitaliy Hriss <vgriss@knu.ua>
2025-04-14 00:35:32 +0300

    Refactor user types and fix typos in seed data
    Updated using directives to include necessary namespaces.
    Refactored user types to general `User` type.
    Added phone numbers and hashed passwords for users.
    Corrected typos in `SchoolClass` and `Subject` names.
    Created and associated `Student`, `Teacher`, and `Parent` instances.
    Added users and instances to context and saved changes.

--- Edu-System-BackEnd/Edu-System-BackEnd.Infrastructure/Persistence/DataSeed.cs
+++ Edu-System-BackEnd/Edu-System-BackEnd.Infrastructure/Persistence/DataSeed.cs
@@ -1,4 +1,6 @@
 ﻿using Edu_System_BackEnd.Edu_System_BackEnd.Core.Entities;
+using Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
 
 namespace Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence
 {
@@ -8,59 +10,81 @@ namespace Edu_System_BackEnd.Edu_System_BackEnd.Infrastructure.Persistence
         {
             if (!context.Users.Any())
             {
-                var schoolClass = new SchoolClass { Name = "11-А" };
+                var passwordHasher = new PasswordHasher<User>();
+
+                var schoolClass = new SchoolClass { Name = "11-A" };
 
                 var studentRole = new Role { Name = "Student" };
                 var teacherRole = new Role { Name = "Teacher" };
                 var parentRole = new Role { Name = "Parent" };
 
                 var mathSubject = new Subject { Name = "Математика" };
-                var histortSubject = new Subject { Name = "Історія" };
+                var historySubject = new Subject { Name = "Історія" };
 
-                var studentUser = new Student
+                var studentUser = new User
                 {
                     FirstName = "Іван",
                     MiddleName = "Петрович",
                     LastName = "Петров",
                     Email = "ivan@example.com",
-                    PasswordHash = "123456",
-                    UserRoles = new List<UserRole> { new UserRole { Role = studentRole } },
-                    SchoolClassId = schoolClass.Id,
-                    SchoolClass = schoolClass
+                    PhoneNumber = "+380123456789",
+                    UserRoles = new List<UserRole> { new UserRole { Role = studentRole } }
                 };
+                studentUser.PasswordHash = passwordHasher.HashPassword(studentUser, "Qwerty123!");
 
-                var teacherUser = new Teacher
+                var teacherUser = new User
                 {
                     FirstName = "Марія",
                     MiddleName = "Василівна",
                     LastName = "Сидоренко",
                     Email = "maria@example.com",
-                    PasswordHash = "123456",
-                    ClassSupervisions = new List<SchoolClass> { schoolClass },
+                    PhoneNumber = "+380987654321",
                     UserRoles = new List<UserRole> { new UserRole { Role = teacherRole } }
                 };
+                teacherUser.PasswordHash = passwordHasher.HashPassword(teacherUser, "Qwerty123!");
 
-                var parentUser = new Parent
+                var parentUser = new User
                 {
                     FirstName = "Олег",
                     MiddleName = "Сергійович",
                     LastName = "Симака",
                     Email = "oleg@example.com",
-                    PasswordHash = "123456",
-                    UserRoles = new List<UserRole> { new UserRole { Role = parentRole } },
-                    StudentParents = new List<StudentParent> { new StudentParent { Student = studentUser } }
+                    PhoneNumber = "+380998877665",
+                    UserRoles = new List<UserRole> { new UserRole { Role = parentRole } }
+                };
+                parentUser.PasswordHash = passwordHasher.HashPassword(parentUser, "Qwerty123!");
+
+                var student = new Student
+                {
+                    User = studentUser,
+                    SchoolClass = schoolClass
+                };
+
+                var teacher = new Teacher
+                {
+                    User = teacherUser,
+                    ClassSupervisions = new List<SchoolClass> { schoolClass }
+                };
+
+                var parent = new Parent
+                {
+                    User = parentUser,
+                    StudentParents = new List<StudentParent>
+                    {
+                        new StudentParent { Student = student }
+                    }
                 };
 
                 context.Roles.AddRange(studentRole, teacherRole, parentRole);
-                context.Students.Add(studentUser);
-                context.Teachers.Add(teacherUser);
-                context.Parents.Add(parentUser);
                 context.SchoolClasses.Add(schoolClass);
-                context.Subjects.AddRange(mathSubject, histortSubject);
+                context.Subjects.AddRange(mathSubject, historySubject);
+                context.Users.AddRange(studentUser, teacherUser, parentUser);
+                context.Students.Add(student);
+                context.Teachers.Add(teacher);
+                context.Parents.Add(parent);
 
                 context.SaveChanges();
             }
         }
     }
-
 }
